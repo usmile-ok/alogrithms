c0f55f5f4a35d7c36de65d261a6f81e78ef80308
ws.zhang <willzhang@hanyuwangxue.com>
2022-02-18 09:40:42 +0800

    LeetCode_Offer_009

---
+++ src/com/usmile/alogrithms/leetcode/LeetCode_Offer_009.java
@@ -0,0 +1,43 @@
+package com.usmile.alogrithms.leetcode;
+
+import java.util.Stack;
+
+/**
+ * 剑指 Offer 09. 用两个栈实现队列
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ *
+ */
+public class LeetCode_Offer_009 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class CQueue1 {
+    Stack<Integer> left = new Stack<>();
+
+    Stack<Integer> right = new Stack<>();
+
+    public void appendTail(int value) {
+        if (!right.isEmpty()) {
+            while(!right.isEmpty()) {
+                left.push(right.pop());
+            }
+        }
+        left.push(value);
+    }
+
+    public int deleteHead() {
+        if (left.isEmpty() && right.isEmpty()) {
+            return -1;
+        }
+        if (right.isEmpty()) {
+            while(!left.isEmpty()) {
+                right.push(left.pop());
+            }
+        }
+
+        return right.pop();
+    }
+}
