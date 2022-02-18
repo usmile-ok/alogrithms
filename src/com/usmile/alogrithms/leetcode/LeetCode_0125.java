a11a70d800f086d1cdb542e7b6bf0c9cddf90312
ws.zhang <willzhang@hanyuwangxue.com>
2022-02-18 14:07:58 +0800

    LeetCode_0027

---
+++ src/com/usmile/alogrithms/leetcode/LeetCode_0125.java
@@ -0,0 +1,51 @@
+package com.usmile.alogrithms.leetcode;
+
+/**
+ * 125. 验证回文串
+ * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ * <p>
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "A man, a plan, a canal: Panama"
+ * 输出: true
+ * 解释："aplanamanacanalpanama" 是回文串
+ * 示例 2:
+ * <p>
+ * 输入: "race a car"
+ * 输出: false
+ * 解释："raceacar" 不是回文串
+ */
+public class LeetCode_0125 {
+}
+
+
+class LeetCode_0125_Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = Character.toLowerCase(s.charAt(i));
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(c);
+            }
+        }
+
+        char[] chars = sb.toString().toCharArray();
+
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right) {
+            if (chars[left] != chars[right]) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
