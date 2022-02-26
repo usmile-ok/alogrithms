e6e918b9c7c0cfc22349c3ffa209b8f85dcabcb1
USmoke <blackmyx@foxmail.com>
2022-02-26 12:32:00 +0800

    binary tree

--- src/com/usmile/alogrithms/learn/bt/TreeNode.java
+++ src/com/usmile/alogrithms/learn/bt/TreeNode.java
@@ -1,20 +1,22 @@
 package com.usmile.alogrithms.learn.bt;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
 import java.util.Stack;
 
 public class TreeNode<T> {
-    T value;
+    T data;
     TreeNode<T> left;
     TreeNode<T> right;
 
-    public TreeNode(T value) {
-        this.value = value;
+    public TreeNode(T data) {
+        this.data = data;
     }
 
-
-    // 前序遍历
+    // 前序遍历（利用栈的特性）-- 迭代
     public static <T> List<T> preOrder(TreeNode<T> root) {
         List<T> result = new ArrayList<>();
         if (null == root) {
@@ -23,9 +25,9 @@ public class TreeNode<T> {
         Stack<TreeNode<T>> nodeStack = new Stack<>();
         nodeStack.push(root);
 
-        while(!nodeStack.isEmpty()) {
+        while (!nodeStack.isEmpty()) {
             TreeNode<T> pop = nodeStack.pop();
-            result.add(pop.value);
+            result.add(pop.data);
             if (null != pop.right) {
                 nodeStack.push(pop.right);
             }
@@ -37,6 +39,21 @@ public class TreeNode<T> {
         return result;
     }
 
+    // 前序遍历（利用栈的特性）-- 递归
+    public static <T> List<T> preOrderR(TreeNode<T> root) {
+        List<T> result = new ArrayList<>();
+        if (null == root) {
+            return result;
+        }
+
+        result.add(root.data);
+        result.addAll(preOrderR(root.left));
+        result.addAll(preOrder(root.right));
+
+        return result;
+    }
+
+    // 中序遍历 - 迭代
     public static <T> List<T> midOrder(TreeNode<T> root) {
         List<T> result = new ArrayList<>();
         if (null == root) {
@@ -44,22 +61,127 @@ public class TreeNode<T> {
         }
         Stack<TreeNode<T>> nodeStack = new Stack<>();
 
-        if (null != root.left) {
-            nodeStack.push(root.left);
-        } else {
-            nodeStack.push(root);
+        TreeNode<T> current = root;
+        while (current != null || !nodeStack.isEmpty()) {
+            while (null != current) {
+                nodeStack.push(current);
+                current = current.left;
+            }
+
+            TreeNode<T> pop = nodeStack.pop();
+            result.add(pop.data);
+
+            // 需要看一下右节点
+            current = pop.right;
         }
 
-        while(!nodeStack.isEmpty()) {
+        return result;
+    }
+
+    // 中序遍历 - 递归
+    public static <T> List<T> midOrderR(TreeNode<T> root) {
+        List<T> result = new ArrayList<>();
+        if (null == root) {
+            return result;
+        }
+
+        result.addAll(midOrderR(root.left));
+        result.add(root.data);
+        result.addAll(midOrderR(root.right));
+
+        return result;
+    }
+
+    // 后序遍历
+    // 思考为什么要对结果进行翻转
+    public static <T> List<T> postOrder(TreeNode<T> root) {
+        ArrayList<T> result = new ArrayList<>();
+        if (null == root) {
+            return result;
+        }
+        Stack<TreeNode<T>> nodeStack = new Stack<>();
+        nodeStack.push(root);
+
+        while (!nodeStack.isEmpty()) {
             TreeNode<T> pop = nodeStack.pop();
+            result.add(0, pop.data);
+//            result.add(pop.data);
             if (null != pop.left) {
                 nodeStack.push(pop.left);
-            } else {
-
             }
             if (null != pop.right) {
                 nodeStack.push(pop.right);
             }
+        }
+//        Collections.reverse(result);
+        return result;
+    }
+
+    // 后序遍历 - 递归
+    public static <T> List<T> postOrderR(TreeNode<T> root) {
+        List<T> result = new ArrayList<>();
+        if (null == root) {
+            return result;
+        }
+
+        result.addAll(postOrderR(root.left));
+        result.addAll(postOrderR(root.right));
+        result.add(root.data);
+
+        return result;
+    }
+
+    // 层序遍历
+    // 利用队列先进先出辅助遍历
+    public static <T> List<T> levelOrder(TreeNode<T> root) {
+        ArrayList<T> result = new ArrayList<>();
+        if (null == root) {
+            return result;
+        }
+
+        Queue<TreeNode<T>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode<T> current = queue.poll();
+            result.add(current.data);
+            if (null != current.left) {
+                queue.offer(current.left);
+            }
+            if (null != current.right) {
+                queue.offer(current.right);
+            }
+        }
+
+        return result;
+    }
+
+    // 层序遍历  - 每层元素单独保存
+    // 利用队列先进先出辅助遍历
+    public static <T> List<List<T>> levelOrder1(TreeNode<T> root) {
+        ArrayList<List<T>> result = new ArrayList<>();
+        if (null == root) {
+            return result;
+        }
+
+        Queue<TreeNode<T>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            // 保存当前层的元素
+            List<T> levels = new ArrayList<>();
+            // 通过当层元素个数从队列取出元素
+            int currentSize = queue.size();
+            for (int i = 0; i < currentSize; i++) {
+                TreeNode<T> current = queue.poll();
+                levels.add(Objects.requireNonNull(current).data);
+                if (null != current.left) {
+                    queue.offer(current.left);
+                }
+                if (null != current.right) {
+                    queue.offer(current.right);
+                }
+            }
+
+            result.add(levels);
         }
 
         return result;
@@ -84,7 +206,7 @@ public class TreeNode<T> {
         node3.right = node7;
         node4.left = node8;
 
-        List<Integer> integers = TreeNode.preOrder(node1);
+        List<Integer> integers = TreeNode.postOrderR(node1);
 
         System.out.println(integers);
 
