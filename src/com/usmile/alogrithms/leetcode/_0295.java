a65cb19d0d4d1430b1bdd9d75f86158c9da1b4a0
ws.zhang <willzhang@hanyuwangxue.com>
2022-03-01 09:31:13 +0800

    295

---
+++ src/com/usmile/alogrithms/leetcode/_0295.java
@@ -0,0 +1,72 @@
+package com.usmile.alogrithms.leetcode;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * 295. 数据流的中位数
+ * 中位数是有序列表中间的数。如果列表长度是偶数，中位数则是中间两个数的平均值。
+ * <p>
+ * 例如，
+ * <p>
+ * [2,3,4] 的中位数是 3
+ * <p>
+ * [2,3] 的中位数是 (2 + 3) / 2 = 2.5
+ * <p>
+ * 设计一个支持以下两种操作的数据结构：
+ * <p>
+ * void addNum(int num) - 从数据流中添加一个整数到数据结构中。
+ * double findMedian() - 返回目前所有元素的中位数。
+ * 示例：
+ * <p>
+ * addNum(1)
+ * addNum(2)
+ * findMedian() -> 1.5
+ * addNum(3)
+ * findMedian() -> 2
+ * 进阶:
+ * <p>
+ * 如果数据流中所有整数都在 0 到 100 范围内，你将如何优化你的算法？
+ * 如果数据流中 99% 的整数都在 0 到 100 范围内，你将如何优化你的算法？
+ */
+public class _0295 {
+}
+
+
+/**
+ * 使用堆的性质
+ */
+class MedianFinder {
+    // 大顶堆， 存储较小元素
+    PriorityQueue<Integer> queMin;
+    // 小顶堆， 存储较大元素
+    PriorityQueue<Integer> queMax;
+
+    public MedianFinder() {
+        queMin = new PriorityQueue<>((a, b) -> (b - a));
+        queMax = new PriorityQueue<>(Comparator.comparingInt(a -> a));
+    }
+
+    public void addNum(int num) {
+        if (queMin.isEmpty() || num <= queMin.peek()) {
+            queMin.offer(num);
+            if (queMax.size() + 1 < queMin.size()) {
+                queMax.offer(queMin.poll());
+            }
+        } else {
+            queMax.offer(num);
+            if (queMax.size() > queMin.size()) {
+                queMin.offer(queMax.poll());
+            }
+        }
+    }
+
+    public double findMedian() {
+        if (queMin.size() > queMax.size()) {
+            return queMin.peek();
+        }
+        return (queMin.peek() + queMax.peek()) / 2.0;
+    }
+}
